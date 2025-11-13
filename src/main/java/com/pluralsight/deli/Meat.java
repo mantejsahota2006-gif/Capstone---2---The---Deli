1ded55d26d67d6b6d8029f6c8249487f99dfd794
mantej <https://github.com/mantejsahota@2006-gif/My-First-Repo.git>
2025-11-13 13:28:42 -0500

    Meat Class

--- src/main/java/com/pluralsight/deli/Meat.java
+++ src/main/java/com/pluralsight/deli/Meat.java
@@ -1,4 +1,15 @@
 package com.pluralsight.deli;
 
 public class Meat extends Topping {
+    public Meat(String name, boolean isExtra) {
+        super(name, isExtra);
+    }
+
+    @Override
+    public double getPrice(int sizeInches) {
+        // Meat is premium: base meat add-on (+ extra add-on if extra)
+        double price = PriceList.meatAddon(sizeInches);
+        if (isExtra) price += PriceList.extraMeatAddon(sizeInches);
+        return price;
+    }
 }

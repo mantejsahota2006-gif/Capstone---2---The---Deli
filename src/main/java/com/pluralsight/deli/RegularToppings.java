7626342082a466d0e1ad8378a4d9fe397f3f6c26
mantej <https://github.com/mantejsahota@2006-gif/My-First-Repo.git>
2025-11-13 13:34:43 -0500

    Regular Toppings Class

--- src/main/java/com/pluralsight/deli/RegularToppings.java
+++ src/main/java/com/pluralsight/deli/RegularToppings.java
@@ -1,4 +1,13 @@
 package com.pluralsight.deli;
 
 public class RegularToppings extends Topping {
+    public RegularToppings(String name, boolean isExtra) {
+        super(name, isExtra);
+    }
+
+    @Override
+    public double getPrice(int sizeInches) {
+        // Regular toppings are included
+        return 0.0;
+    }
 }

f19e2039508402a4d3c4395b2808bfbda1a1e53c
mantej <https://github.com/mantejsahota@2006-gif/My-First-Repo.git>
2025-11-13 14:38:57 -0500

    Drink class

--- src/main/java/com/pluralsight/deli/Drink.java
+++ src/main/java/com/pluralsight/deli/Drink.java
@@ -1,4 +1,16 @@
 package com.pluralsight.deli;
 
 public class Drink extends OtherProducts {
+    private final String size;   // Small, Medium, Large
+    private final String flavor; // Different flavors
+
+    public Drink(String size, String flavor) {
+        super("Drink", PriceList.drinkPrice(size));
+        this.size = size;
+        this.flavor = flavor;
+        this.name = "Drink (" + size + " " + flavor + ")";
+    }
+
+    public String getSize() { return size; }
+    public String getFlavor() { return flavor; }
 }

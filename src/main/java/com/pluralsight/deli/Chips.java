ae3be531a5f2123d47ac5007399e7127afb7ace1
mantej <https://github.com/mantejsahota@2006-gif/My-First-Repo.git>
2025-11-13 14:42:09 -0500

    Chips class

--- src/main/java/com/pluralsight/deli/Chips.java
+++ src/main/java/com/pluralsight/deli/Chips.java
@@ -1,4 +1,19 @@
 package com.pluralsight.deli;
 
 public class Chips extends OtherProducts {
+    private final boolean isIncluded;
+
+    // If you add Chips to the order, pass isIncluded=true.
+    public Chips(boolean isIncluded) {
+        super("Chips", isIncluded ? PriceList.CHIPS_PRICE : 0.0);
+        this.isIncluded = isIncluded;
+        this.name = "Chips" + (isIncluded ? "" : " (not added)");
+    }
+
+    public boolean isIncluded() { return isIncluded; }
+
+    @Override
+    public String toString() {
+        return isIncluded ? "Chips $" + String.format("%.2f", price) : "Chips (not added)";
+    }
 }
